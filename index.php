f73a60ba0844532ea51a69eb3da9350646e0ec59
Felipe Sales <felipesales007@hotmail.com>
2019-12-03 11:17:59 -0300

    aws

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!-- Redirecionamento para página principal -->
 <?php
-    header("location: https://www.felipesales.com.br/view/curriculo/perfil");
+    header("location: https://www.felipesales.com.br/site/view/curriculo/perfil");
     require_once("controller/curriculo/notificacao.php");
     $_SESSION["success"] = "Seja bem vindo!";
 ?>
